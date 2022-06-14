6090ba8186f525cf07f714922fc11880a0901653
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-14 21:17:33 +0700

    Menambahkan Component Product Offer List

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage/LandingPage';
+import OfferList from '../pages/OfferList/OfferList';
 
 function IndexRoute() {
   return (
     <Fragment>
         <Routes>
             <Route path='/' element={<LandingPage />}/>
+            <Route path='/daftarpenawar' element={<OfferList />}/>
         </Routes>
     </Fragment>
   );
