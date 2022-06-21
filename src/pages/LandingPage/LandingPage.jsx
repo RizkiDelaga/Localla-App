736e70a19a75a19ba5d6c4000df97f5105f7b870
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-21 17:02:25 +0700

    Menambahkan Card Product

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,27 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import CardProduct from '../../components/CardProduct/CardProduct';
+import Navbar from '../../components/Navbar/Navbar';
+import GroupButton from "../../components/ButtonGroup/ButtonGroup";
+import style from './LandingPage.module.css';
+
+import White_Plus_Icon from '../../assets/icons/White_Plus_Icon.png';
+
 
 function LandingPage() {
   return (
     <Fragment>
-        <h1>Hello World!!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quisquam neque doloremque vero nam perferendis, dicta ratione error nihil! Molestiae praesentium placeat eaque deserunt eos amet culpa voluptate laboriosam eum.</p>
+        <Navbar logo={true} search={true} mobileMenu={true} login={true} desktopMenu={true} transparentFade={true} />
+
+        <Container style={{marginTop: '100px', marginBottom: '50px'}}>
+          <h1>Hello World!!</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quisquam neque doloremque vero nam perferendis, dicta ratione error nihil! Molestiae praesentium placeat eaque deserunt eos amet culpa voluptate laboriosam eum.</p>
+          <br />
+          <GroupButton />
+          <CardProduct/>
+        </Container>
+
+        <button className={`d-flex align-items-center ${style['btn-add-product']}`}><img src={White_Plus_Icon} className={`me-2`} style={{height: '30px'}} alt="" />Jual</button>
     </Fragment>
   );
 }
