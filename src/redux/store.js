4a6bf64cc009946ba3fecbe83f65529c6f7abcd8
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-26 23:18:21 +0700

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import combineReducers from './reducers/rootReducer';
+import rootReducer from './reducers/rootReducer';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    combineReducers,
+    rootReducer,
     composeEnhancer(applyMiddleware(thunk))
 );
 
