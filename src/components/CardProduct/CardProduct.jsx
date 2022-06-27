35fdd56bb65b3911fb38724d9e287b700da87b46
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-27 12:44:09 +0700

    Menambah basic Redux

--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -3,18 +3,18 @@ import style from './CardProduct.module.css';
 import Default_Product_Image from '../../assets/images/image1.jpg';
 
 
-function CardProduct() {
-
+function CardProduct({ product }) {
+  console.log(product);
   return (
     <Fragment>
-        <div className={`mx-auto ${style['card-product']}`}>
-            <img src={Default_Product_Image} className={`${style['img-container']}`} alt="" />
-            <div className={`my-2`}>
-                <h6 className={`m-0`}>Jam Tangan Casio Lorem ipsum, dolor sit amet</h6>
-                <p className={`m-0 py-2 text-secondary`} style={{fontSize: '14px'}}>Aksesoris</p>
-                <h6 className={`m-0`}>Rp 250.000</h6>
-            </div>
-        </div>
+          <div className={`mx-auto ${style['card-product']}`}>
+              <img src={product.image} className={`${style['img-container']}`} alt="" />
+              <div className={`my-2`}>
+                  <h6 className={`m-0`}>{product.title}</h6>
+                  <p className={`m-0 py-2 text-secondary`} style={{fontSize: '14px'}}>Aksesoris</p>
+                  <h6 className={`m-0`}>Rp {product.price}</h6>
+              </div>
+          </div>
     </Fragment>
   );
 }
