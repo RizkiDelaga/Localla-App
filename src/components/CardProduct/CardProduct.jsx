2e674dc3fb89bc10ecbbbaa75ceca35ad2df7e1a
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-06-27 12:58:12 +0700

    Perbaiki bugs

--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -5,14 +5,15 @@ import Default_Product_Image from '../../assets/images/image1.jpg';
 
 function CardProduct({ product }) {
   console.log(product);
+  
   return (
     <Fragment>
           <div className={`mx-auto ${style['card-product']}`}>
-              <img src={product.image} className={`${style['img-container']}`} alt="" />
+              <img src={product? product.image : Default_Product_Image} className={`${style['img-container']}`} alt="" />
               <div className={`my-2`}>
-                  <h6 className={`m-0`}>{product.title}</h6>
+                  <h6 className={`m-0`}>{product? product.title : "Lorem ipsum dolor sit"}</h6>
                   <p className={`m-0 py-2 text-secondary`} style={{fontSize: '14px'}}>Aksesoris</p>
-                  <h6 className={`m-0`}>Rp {product.price}</h6>
+                  <h6 className={`m-0`}>Rp {product? product.price : 125000}</h6>
               </div>
           </div>
     </Fragment>
