63be771b9aadc1fe56e0ba0b3bb2bedbb228981e
daruuuu <alfiandaruh@gmail.com>
2022-06-27 14:21:31 +0700

    add mobile banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,22 +8,22 @@ const Banner = () => {
   return (
     <Container
       fluid
-      className="d-flex align-items-center"
-      style={{ padding: "100px 0 0 " }}
+      className={`d-flex align-items-center justify-content-center ${styles.banner}`}
     >
       <div className={`${styles.box} ${styles.box1}`}></div>
       <div className={`${styles.box2}`}>
-        <div style={{ paddingLeft: "80px" }}>
+        <div className={styles["banner-text"]}>
           <h2 style={{ fontWeight: "700" }}>Bulan Ramadhan Banyak diskon!</h2>
           <p style={{ fontWeight: "400", margin: "0" }}>Diskon Hingga</p>
           <p style={{ fontWeight: "500", color: "#FA2C5A", fontSize: "32px" }}>
             60%
           </p>
         </div>
-        <img src={gift} alt="" style={{ transform: "translateX(57px)" }} />
-        <img src={img1} alt="" className={styles.shadow} />
+        <img src={gift} alt="" className={styles.img1} />
+        <img src={img1} alt="" className={styles.img2} />
       </div>
       <div className={`${styles.box} ${styles.box3}`}></div>
+      <div className={`${styles["banner-fade"]}`} />
     </Container>
   );
 };
