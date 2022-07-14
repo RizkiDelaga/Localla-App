bb91bbb31f2a2d89321ce011d89a7900fbce2af9
daruuuu <alfiandaruh@gmail.com>
2022-07-14 16:07:20 +0700

    Add Hero Section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,52 +1,63 @@
-import React, { useState } from "react";
-import { Carousel } from "react-bootstrap";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper";
+
 import shop from "../../assets/images/shop.jpg";
-import shopping from "../../assets/images/shopping.jpg";
 import shop1 from "../../assets/images/shop1.jpg";
+import shopping from "../../assets/images/shopping.jpg";
+
 import styles from "./Banner.module.css";
 
 const Banner = () => {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item className={styles["banner-img"]}>
-        <img className="d-block w-100" src={shop} alt="First slide" />
-        <Carousel.Caption
-          className={styles["banner-caption"]}
-          style={{ textAlign: "left" }}
-        >
-          <h3>Local with standart International</h3>
-          <p>Localla offering local brand with international quality</p>
-          <button className={styles["banner-btn"]}>Get Started</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item className={styles["banner-img"]}>
-        <img className="d-block w-100" src={shopping} alt="Second slide" />
-        <Carousel.Caption className={styles["banner-caption"]}>
-          <h3>Think Globally Act Locally</h3>
-          <p>Globalization must be grow up with localization</p>
-          <button className={styles["banner-btn"]}>Get Started</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item className={styles["banner-img"]}>
-        <img className="d-block w-100" src={shop1} alt="Third slide" />
-        <Carousel.Caption
-          className={styles["banner-caption"]}
-          style={{ textAlign: "left" }}
-        >
-          <h3>Support your Local</h3>
-          <p>Put your money where your house is</p>
-          <button className={styles["banner-btn"]}>Get Started</button>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Swiper
+      navigation={true}
+      mousewheel={{
+        invert: true,
+      }}
+      rewind={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay, Navigation]}
+      className={`${styles["swipper"]}`}
+    >
+      <SwiperSlide style={{ width: "100%" }}>
+        <div style={{ position: "relative" }}>
+          <div className={styles["swiper-caption"]}>
+            <h3>Local With International Quality</h3>
+            <p>Localla offering local brand with international quality</p>
+            <button className={styles["banner-btn"]}>Shopping Now</button>
+          </div>
+          <img className={styles["swiper-img"]} alt="" src={shop} />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide style={{ width: "100%" }}>
+        <div style={{ position: "relative" }}>
+          <div className={styles["swiper-caption1"]}>
+            <h3>Think Globally Act Locally</h3>
+            <p>Globalization must be grow up with localization</p>
+            <button className={styles["banner-btn"]}>Shopping Now</button>
+          </div>
+          <img className={styles["swiper-img"]} alt="" src={shop1} />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide style={{ width: "100%" }}>
+        <div style={{ position: "relative" }}>
+          <div className={styles["swiper-caption"]}>
+            <h3>Support Your Local</h3>
+            <p>Put your money where your house is</p>
+            <button className={styles["banner-btn"]}>Shopping Now</button>
+          </div>
+          <img className={styles["swiper-img"]} alt="" src={shopping} />
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
