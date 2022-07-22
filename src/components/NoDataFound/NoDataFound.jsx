6b36949640df13a8110e853af1bd818caba173d7
daruuuu <alfiandaruh@gmail.com>
2022-07-22 21:32:24 +0700

    testing

--- src/components/NoDataFound/NoDataFound.jsx
+++ src/components/NoDataFound/NoDataFound.jsx
@@ -1,12 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import No_Data_Image from '../../assets/images/No_Data_Image.png';
+import No_Data_Image from "../../assets/images/No_Data_Image.png";
 
 function NoDataFound() {
   return (
     <Fragment>
       <div className="text-center">
-        <img src={No_Data_Image} alt="" className="w-100" style={{ maxWidth: '400px' }} />
+        <img
+          src={No_Data_Image}
+          alt="no-data"
+          className="w-100"
+          style={{ maxWidth: "400px" }}
+        />
       </div>
     </Fragment>
   );
