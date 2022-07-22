485a3ba5a8fc1bc18ed067ce1ff26b7572b1ddb9
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2022-07-22 21:13:33 +0700

    Update progres

--- src/redux/Actions/WishListAction.js
+++ src/redux/Actions/WishListAction.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {
-    UPDATE_WISHLIST
+    UPDATE_WISHLIST, GET_MY_WISHLIST
 } from "../types";
 
 export const updateWishList = (productID) => {
@@ -25,6 +25,34 @@ export const updateWishList = (productID) => {
             console.log("err.. transaction ", err);
             dispatch({
                 type: `${UPDATE_WISHLIST}_ERROR`,
+                error: err.message
+            })
+        })
+    }
+}
+
+export const getMyWishList = () => {
+    return (dispatch) => {
+        dispatch({
+            type: `${GET_MY_WISHLIST}_LOADING`
+        });
+
+        axios({
+            method: 'GET',
+            url: `https://localla-api.herokuapp.com/api/v1/user/wishlist`,
+            headers: {
+                'authorization': `${localStorage.getItem('access_token')}`
+            }
+        }).then((res) => {
+            console.log("data.. transaction ", res);
+            dispatch({
+                type: `${GET_MY_WISHLIST}_FULFILLED`,
+                payload: res.data.data
+            });
+        }).catch((err) => {
+            console.log("err.. transaction ", err);
+            dispatch({
+                type: `${GET_MY_WISHLIST}_ERROR`,
                 error: err.message
             })
         })
